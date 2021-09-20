98c8e5561b299b9ce01be79b2bc8820259df0119
Bruno Belarte <bruno.belarte@gmail.com>
2021-09-20 14:43:14 +0200

    WIP: change package manager to packer

--- nvim/lua/plugins/paq-nvim.lua
+++ nvim/lua/plugins/paq-nvim.lua
@@ -2,32 +2,170 @@
 -- Plugin manager configuration file
 -----------------------------------------------------------
 
--- Bootstrapping
 local fn = vim.fn
+--local buf_read = "BufRead"
+--local buf_enter = "BufEnter"
 
-local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+-- Bootstrapping
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-   fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+  fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+  vim.api.nvim_command("packadd packer.nvim")
 end
-
--- Plugin manager: paq-nvim
--- https://github.com/savq/paq-nvim
-
-vim.cmd 'packadd paq-nvim'            -- load paq
-local paq = require('paq-nvim').paq   -- import module with `paq` function
 
 -- Add packages
 -- for package info see: init.lua (Lua modules)
 require 'paq' {
   'savq/paq-nvim';  -- let paq manage itself
 
-  'jremmen/vim-ripgrep';
-  'vim-airline/vim-airline';
-  'junegunn/fzf';
-  'junegunn/fzf.vim';
-  'tpope/vim-fugitive';
-  'airblade/vim-gitgutter';
-  'vimwiki/vimwiki';
-  'Olical/conjure';
 }
+
+local plugins = {
+  ["wbthomason/packer.nvim"] = {},
+  ['jremmen/vim-ripgrep'] = {},
+  ['vim-airline/vim-airline'] = {},
+--  ['junegunn/fzf'] = {},
+--  ['junegunn/fzf.vim'] = {},
+  ['tpope/vim-fugitive'] = {},
+  ['airblade/vim-gitgutter'] = {},
+  ['vimwiki/vimwiki'] = {},
+  ["Olical/conjure"] = {
+    ft = "clojure",
+  },
+  ["guns/vim-sexp"] = {
+    ft = "clojure",
+  },
+  ["eraserhd/parinfer-rust"] = {
+    ft = "clojure",
+    run = "cargo build --release",
+  },
+  ["nvim-treesitter/nvim-treesitter"] = {
+    run = ":TSUpdate",
+    branch = "0.5-compat",
+    config = function()
+      require("treesitter")
+    end,
+  },
+  ["nvim-telescope/telescope.nvim"] = {
+    cmd = "Telescope",
+    requires = {
+      "nvim-lua/popup.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+  },
+--  ["kyazdani42/nvim-web-devicons"] = {},
+--  ["Pocco81/Catppuccino.nvim"] = { -- colorscheme
+--    after = "packer.nvim",
+--    config = function()
+--      require("colorscheme")
+--    end,
+--  },
+--  ["kyazdani42/nvim-tree.lua"] = {
+--    cmd = "NvimTreeToggle",
+--  },
+--  ["akinsho/nvim-bufferline.lua"] = {
+--    after = "nvim-web-devicons",
+--    config = function()
+--      require("bufferline").setup({})
+--    end,
+--  },
+--  ["tamton-aquib/staline.nvim"] = {
+--    after = "nvim-bufferline.lua",
+--    config = function()
+--      require("statusline")
+--    end,
+--  },
+--  ["hrsh7th/nvim-cmp"] = {
+--    requires = {
+--      "hrsh7th/cmp-buffer",
+--      "hrsh7th/cmp-nvim-lsp",
+--      "hrsh7th/cmp-nvim-lua",
+--      "hrsh7th/cmp-path",
+--    },
+--    config = function()
+--      require("cmp").setup({
+--        sources = {
+--          { name = "nvim_lsp" },
+--          { name = "buffer" },
+--          { name = "nvim_lua" },
+--          { name = "path" },
+--        },
+--      })
+--    end,
+--  },
+--  ["neovim/nvim-lspconfig"] = {
+--    event = buf_enter,
+--    config = function()
+--      require("lsp")
+--    end,
+--  },
+--  ["lewis6991/gitsigns.nvim"] = {
+--    event = buf_read,
+--    requires = "nvim-lua/plenary.nvim",
+--    config = function()
+--      require("gitsigns").setup()
+--    end,
+--  },
+--  ["famiu/bufdelete.nvim"] = {
+--    event = buf_enter,
+--  },
+--  ["hashivim/vim-terraform"] = {
+--    ft = "terraform",
+--  },
+--  ["folke/which-key.nvim"] = {
+--    event = "VimEnter",
+--    config = function()
+--      require("which-key").setup({})
+--    end,
+--  },
+--  ["p00f/nvim-ts-rainbow"] = {
+--    event = buf_read,
+--    requires = "neovim/nvim-lspconfig",
+--    config = function()
+--      require("nvim-treesitter.configs").setup({
+--        rainbow = {
+--          enable = true,
+--          extended_mode = true,
+--        },
+--      })
+--    end,
+--  },
+--  ["mfussenegger/nvim-lint"] = {
+--    event = buf_read,
+--    config = function()
+--      local sh = { "shellcheck" }
+--
+--      vim.cmd([[autocmd BufEnter,InsertLeave,TextChanged * lua require("lint").try_lint()]])
+--
+--      require("lint").linters_by_ft = {
+--        zsh = sh,
+--        sh = sh,
+--        dockerfile = { "hadolint" },
+--      }
+--    end,
+--  },
+--  ["blackCauldron7/surround.nvim"] = {
+--    config = function()
+--      require("surround").setup({
+--        mappings_style = "surround",
+--      })
+--    end,
+--  },
+}
+
+return require("packer").startup(function(use)
+  for plugin, conf in pairs(plugins) do
+    use({
+      plugin,
+      event = conf.event,
+      cmd = conf.cmd,
+      requires = conf.requires,
+      run = conf.run,
+      config = conf.config,
+      ft = conf.ft,
+      branch = conf.branch,
+      as = conf.as,
+    })
+  end
+end)
