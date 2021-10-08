e4ce4908cf4c8fb4d951744935433587890c29df
Bruno Belarte <bruno.belarte@gmail.com>
2021-10-08 09:20:36 +0200

    nvim: use LSP config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -4,6 +4,16 @@ lsps = {
   "clojure_lsp", -- brew install clojure-lsp/brew/clojure-lsp-native
 }
 
-for _, v in pairs(lsps) do
-  lspconfig[v].setup({})
+
+for _, lsp in pairs(lsps) do
+  conf = {}
+
+  if type(lsp) == "table" then
+    for custom_lsp, custom_conf in pairs(lsp) do
+      lsp = custom_lsp
+      conf = custom_conf
+    end
+  end
+
+  lspconfig[lsp].setup(conf)
 end
