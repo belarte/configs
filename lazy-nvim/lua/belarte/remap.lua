67456aeaa87e9f414eb92a75846c80d164ff991b
Bruno Belarte <bruno.belarte@gmail.com>
2023-10-15 19:38:12 +0200

    nvim: quickfix shortcuts

--- lazy-nvim/lua/belarte/remap.lua
+++ lazy-nvim/lua/belarte/remap.lua
@@ -19,3 +19,6 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "]q", ":cn<CR>")
+vim.keymap.set("n", "[q", ":cp<CR>")
