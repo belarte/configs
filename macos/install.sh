550edbbe20a93de84e0b9f3110d7c902590839c2
Bruno Belarte <bruno.belarte@gmail.com>
2024-10-26 14:04:11 +0200

    macos: fix shell in install script

--- macos/install.sh
+++ macos/install.sh
@@ -7,7 +7,7 @@ then
     echo '' >> ~/.zshrc
     echo 'source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
     echo '' >> ~/.zshrc
-    echo 'eval "$(direnv hook bash)"' >> ~/.zshrc
+    echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
     echo '' >> ~/.zshrc
     echo 'source <(fzf --zsh)' >> ~/.zshrc
 
